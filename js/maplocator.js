632718b3ad17fd33e07eb2c4846743396f36afc7
Abhijith A Nair <abhijith.nair@experionglobal.local>
2023-11-10 23:06:39 +0530

    fix-fixed filter and toggle buttons

--- js/maplocator.js
+++ js/maplocator.js
@@ -1,60 +1,83 @@
+// Global variable to store the Leaflet map
 var map;
+
+// Event listener for when the DOM content is loaded
 document.addEventListener("DOMContentLoaded", function () {
+  // Get DOM elements for toggle button, search section, and map section
   const toggleButton = document.getElementById("toggle-search");
   const searchSection = document.querySelector(".search");
   const mapSection = document.querySelector(".map");
 
+  // Variable to track the visibility of the search section
   let isSearchVisible = true;
 
+  // Toggle button click event to show/hide the search section
   toggleButton.addEventListener("click", function () {
     if (isSearchVisible) {
       // Hide the search section
       searchSection.style.display = "none";
       mapSection.style.flex = 9.5; // Expand the map to full width
-      // mapSection.style.width = "1080px";
     } else {
       // Show the search section
       searchSection.style.display = "block";
       mapSection.style.flex = 1;
       mapSection.style.width = "1080px";
-      //mapSection.style.width = 150; // Restore the map width
     }
     isSearchVisible = !isSearchVisible;
   });
 
-  // function changeToggleButtonText() {
-  //   var button = document.getElementById("toggle-search");
+  // Function to change toggle button text
+  function changeToggleButtonText() {
+    var button = document.getElementById("toggle-search");
 
-  //   if (button.innerHTML === "&gt;") {
-  //     button.innerHTML = "&lt;";
-  //   } else {
-  //     button.innerHTML = "&gt;";
-  //   }
-  // }
+    if (button.innerHTML === "&gt;") {
+      button.innerHTML = "&lt;";
+    } else {
+      button.innerHTML = "&gt;";
+    }
+  }
 
+  // Add click event listener for the toggle button
+  document.getElementById("toggle-search").onclick = changeToggleButtonText;
+
+  // Initialize the Leaflet map with default view
   map = L.map("map-section").setView([36.08, -86.6], 13);
 
+  // Add OpenStreetMap tile layer to the map
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
 
-  // Add a marker with the custom icon to the map
+  // Add a marker with a custom icon to the map (not implemented yet)
 });
-fetch("https://dummyjson.com/users")
-  .then((response) => response.json())
+
+// Fetch data from an API endpoint
+// Wrap the fetch operation in a promise
+const fetchData = async () => {
+  try {
+    const response = await fetch("https://dummyjson.com/users");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(`Error fetching data: ${error.message}`);
+  }
+};
+
+// Use the fetchData function
+fetchData()
   .then((data) => {
     console.log(data);
     var customIcon = L.icon({
       iconUrl:
         "https://images.ctfassets.net/27hc2gu70btq/4wCs01Cgbzv2gycua3IHfv/48e087bddc5ee9639d574ae291488069/storeSelected.svg",
-      iconSize: [38, 95], // size of the icon
-      shadowSize: [50, 64], // size of the shadow
-      iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-      shadowAnchor: [4, 62], // the same for the shadow
-      popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+      iconSize: [38, 95],
+      shadowSize: [50, 64],
+      iconAnchor: [22, 94],
+      shadowAnchor: [4, 62],
+      popupAnchor: [-3, -76],
     });
-    const user = data.users; //name,location,distance
+    const user = data.users;
     user.forEach((element, index) => {
       console.log(element);
       var storescontainer = document.querySelector(".stores");
@@ -91,7 +114,88 @@ fetch("https://dummyjson.com/users")
       storescontainerdiv.appendChild(breakline);
       storescontainer.appendChild(storescontainerdiv);
     });
+  })
+  .catch((error) => {
+    console.error(error.message);
   });
+
+// Make showstoredetails asynchronous
+async function showstoredetails(
+  namepass,
+  locationpass,
+  distancepass,
+  latitudepass,
+  longitudepass
+) {
+  // Clear existing content
+  document.querySelector(".stores").innerHTML = "";
+
+  // Create elements for store details
+  const name = document.createElement("h2");
+  const location = document.createElement("p");
+  const distance = document.createElement("p");
+
+  // Populate store details
+  name.textContent = namepass;
+  location.textContent = locationpass;
+  distance.textContent = distancepass;
+
+  // Add store details to DOM
+  const storesContainer = document.querySelector(".stores");
+  storesContainer.appendChild(name);
+  storesContainer.appendChild(location);
+  storesContainer.appendChild(distance);
+
+  // Create a div for the newsletter subscription form
+  const newsletterDiv = document.createElement("div");
+  newsletterDiv.className = "newsletter-form";
+
+  // Create heading for the newsletter
+  const newsletterHeading = document.createElement("h2");
+  newsletterHeading.textContent = "Subscribe to our newsletter";
+
+  // Create form elements
+  const emailInput = document.createElement("input");
+  emailInput.type = "email";
+  emailInput.placeholder = "Enter your email";
+  emailInput.id = "subscribe-email";
+
+  const subscribeButton = document.createElement("button");
+  subscribeButton.textContent = "Subscribe";
+
+  subscribeButton.addEventListener("click", async () => {
+    try {
+      // Validate email format
+      const email = emailInput.value;
+      if (validateEmail(email)) {
+        alert(`Successfully subscribed with email: ${email}`);
+      } else {
+        alert("Invalid email format. Please enter a valid email address.");
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  });
+
+  // Add form elements to the newsletter div
+  newsletterDiv.appendChild(newsletterHeading);
+  newsletterDiv.appendChild(emailInput);
+  newsletterDiv.appendChild(subscribeButton);
+
+  // Add newsletter div to the DOM
+  storesContainer.appendChild(newsletterDiv);
+
+  // Update map
+  map.panTo([latitudepass, longitudepass]);
+}
+
+// Function to validate email format
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+// Function to show store details
 function showstoredetails(
   namepass,
   locationpass,
@@ -152,7 +256,7 @@ function showstoredetails(
   // Add newsletter div to the DOM
   storesContainer.appendChild(newsletterDiv);
 
-  // Update map
+  // Update map view to the selected store location
   map.panTo([latitudepass, longitudepass]);
 }
 
@@ -162,6 +266,7 @@ function validateEmail(email) {
   return emailRegex.test(email);
 }
 
+// Function to get the user's current location
 function Locateme() {
   if ("geolocation" in navigator) {
     // Request the user's current location
@@ -180,11 +285,16 @@ function Locateme() {
   }
 }
 
+// Event listener for the search button
 document.getElementById("search-button").addEventListener("click", function () {
+  // Get the search input value and convert to lowercase
   var searchInput = document.getElementById("search-input").value.toLowerCase();
   console.log(searchInput);
+  // Call the searchStores function with the search input
   searchStores(searchInput);
 });
+
+// Function to filter and display stores based on keyword
 function searchStores(keyword) {
   var storesContainer = document.querySelector(".stores");
   var storeItems = storesContainer.getElementsByClassName("store-item");
@@ -206,11 +316,16 @@ function searchStores(keyword) {
     }
   }
 }
+
+// Event listener for when the DOM content is loaded (again)
 document.addEventListener("DOMContentLoaded", function () {
+  // Get DOM elements for filter button and filter form
   var filterButton = document.getElementById("show-filter-form");
   var filterForm = document.querySelector(".filter-form");
 
+  // Check if filter button and filter form exist
   if (filterButton && filterForm) {
+    // Event listener for filter button click to toggle filter form visibility
     filterButton.addEventListener("click", function () {
       filterForm.style.display =
         filterForm.style.display === "none" || filterForm.style.display === ""
@@ -218,7 +333,8 @@ document.addEventListener("DOMContentLoaded", function () {
           : "none";
     });
   }
-  // Handle the apply filter button click
+
+  // Event listener for the apply filter button click
   document
     .getElementById("apply-filter")
     .addEventListener("click", function () {
@@ -232,4 +348,3 @@ document.addEventListener("DOMContentLoaded", function () {
       searchStores(selectedFilterType);
     });
 });
-// Existing code...
